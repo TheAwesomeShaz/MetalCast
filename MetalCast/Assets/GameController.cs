3e58133f5232a892f8e76f6d29f41139cbb9fb7e
Knightmare-sama <51862748+Knightmare-sama@users.noreply.github.com>
2020-09-09 09:04:45 +0530

    Added basic scene

---
+++ MetalCast/Assets/GameController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public Animator stampAnim;
+    public GameObject lavaParticles;
+    public GameObject removableThing;
+    public GameObject negativeStamp;
+    public bool startFill;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        removableThing.SetActive(true);
+        negativeStamp.SetActive(false);
+        lavaParticles.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Stamp()
+    {
+        stampAnim.SetTrigger("Stamp");
+
+        StartCoroutine(SetActiveAfterTime(negativeStamp, 1f));
+        StartCoroutine(SetInActiveAfterTime(removableThing, 1f));
+
+        StartCoroutine(SetActiveAfterTime(lavaParticles, 1.2f));
+        startFill = true;
+
+
+    }
+
+    IEnumerator SetInActiveAfterTime(GameObject thing, float time)
+    {
+        yield return new WaitForSeconds(time);
+        thing.SetActive(false);
+    }
+
+    IEnumerator SetActiveAfterTime(GameObject thing, float time)
+    {
+        yield return new WaitForSeconds(time);
+        thing.SetActive(true);
+
+    }
+}
+
+
---
+++ MetalCast/Assets/Lava.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour
+{
+    public float lavaSpeed = 2f;
+    GameController gc;
+
+    private void Start()
+    {
+        gc = FindObjectOfType<GameController>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (gc.startFill)
+        {
+
+            if (transform.localScale.x < 20)
+                transform.localScale = Vector3.Lerp(transform.localScale,
+                    new Vector3(20f, transform.localScale.y, transform.localScale.z), lavaSpeed * Time.deltaTime);
+        }
+    }
+
+    
+}
